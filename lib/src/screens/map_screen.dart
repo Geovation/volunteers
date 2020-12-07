b923ea7bdef499dd91f58867c041fade55395a31
Joy Shan-Chun Kuo <shanchun.kuo@geovation.uk>
2020-12-07 17:40:23 +0000

    Add custom app bar

--- lib/src/screens/map_screen.dart
+++ lib/src/screens/map_screen.dart
@@ -1,31 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:provider/provider.dart';
 import 'package:volunteers/src/config.dart';
+import 'package:volunteers/src/widgets/app_bar.dart';
 import 'package:volunteers/src/widgets/map_control_plugin.dart';
 import 'package:volunteers/src/widgets/attribution_plugin.dart';
-import 'package:volunteers/src/utils/helpers/scaffold_helper.dart';
-import 'package:volunteers/src/core/viewmodels/app_state.dart';
 
 class MapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Volunteers'),
-        leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () => RootScaffold.openDrawer(context)),
-        actions: <Widget>[
-          if (FirebaseAuth.instance.currentUser != null)
-            IconButton(
-                icon: Icon(Icons.logout),
-                tooltip: 'Log out',
-                onPressed: () => context.read<AppState>().signOut())
-        ],
-      ),
+      appBar: CustomAppBar(title: 'Volunteers'),
       body: FlutterMap(
         options: MapOptions(
           center: LatLng(51.5, -0.09),
---
+++ lib/src/widgets/app_bar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:volunteers/src/core/viewmodels/app_state.dart';
+import 'package:volunteers/src/utils/helpers/scaffold_helper.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  CustomAppBar({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(title),
+      leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () => RootScaffold.openDrawer(context)),
+      actions: <Widget>[
+        if (FirebaseAuth.instance.currentUser != null)
+          IconButton(
+              icon: Icon(Icons.logout),
+              tooltip: 'Log out',
+              onPressed: () {
+                context.read<AppState>().signOut();
+                Navigator.popUntil(context, ModalRoute.withName('/'));
+              })
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
